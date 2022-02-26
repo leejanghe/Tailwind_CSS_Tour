f64af746d6c5d5114ecb754bee7b1bbf403f0f39
leejanghe <babybye777@gmail.com>
2022-02-26 17:01:45 +0900

    test drive part three

--- tailwind-css/pages/index.tsx
+++ tailwind-css/pages/index.tsx
@@ -46,7 +46,39 @@ const Home: NextPage = () => {
       </div>
 
 
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⬅️</span>
+          <div className="space-x-3">
+             <span>⭐️ 4.9</span>
+             <span className="shadow-xl p-2 rounded-md">❤️</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5" />
+          <div className="flex flex-col">
+            <span className="font-medium text-xl">Swoon Lounge</span>
+            <span className="text-xs text-gray-500">Chair</span>
+            <div className="mt-3 mb-5 flex justify-between items-center">
+              <div>
+              <input type="radio"/>
+              <input type="radio"/>
+              <input type="radio"/>
+              </div>
+              <div className="flex items-center space-x-5">
+                <button className="p-1.5 rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">-</button>
+                <span>1</span>
+                <button className="p-1.5 rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">+</button>
+              </div>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="font-medium text-2xl">$450</span>
+              <button className="bg-blue-500 py-2 px-8 text-xs text-center text-white rounded-lg">Add to cart</button>
+            </div>
+          </div>
+       
+      </div>
+
+
       <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
     </div>
   )
