8e5df3e7f0e0bbf3639378c97d0318039d03b1de
leejanghe <babybye777@gmail.com>
2022-02-27 17:02:24 +0900

    peer

--- tailwind-css/pages/index.tsx
+++ tailwind-css/pages/index.tsx
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
       </div>
 
 
-      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
       <div className="bg-blue-500 p-6 pb-14">
           <span className="text-white text-2xl">Profile</span>
         </div>
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 bg-zinc-300 rounded-full group-hover:bg-red-400 transition-colors" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$340</span>
@@ -103,7 +103,20 @@ const Home: NextPage = () => {
       </div>
 
 
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl">
+        <form className="flex flex-col space-y-2 p-5">
+          <input type="text" required placeholder="username"
+          className="border p-1 peer border-gray-400"
+          />
+           <span className="hidden peer-invalid:block peer-invalid:text-red-500">
+            This input is invalid
+          </span>
+          <span className="hidden peer-valid:block peer-valid:text-teal-500">
+            Awesome username
+          </span>
+          <input type="submit" value="Login" className="bg-white"/>
+        </form>
+      </div>
     </div>
   )
 }
