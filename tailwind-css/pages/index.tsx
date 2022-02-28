c02d7829183e36a477818ac9e20399cfdfff4a62
leejanghe <babybye777@gmail.com>
2022-02-28 16:41:58 +0900

    responsive part 2

--- tailwind-css/pages/index.tsx
+++ tailwind-css/pages/index.tsx
@@ -3,8 +3,8 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
+    <div className="bg-slate-400 xl:place-content-center py-20 px-10 grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="bg-white p-6 rounded-3xl shadow-xl flex:flex-col justify-between">
         <span className="font-semibold text-3xl">Select Item</span>
         {/* <div className="flex justify-between my-2">
           <span className="text-gray-500">Grey Chair</span>
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
 
 
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
-      <div className="bg-blue-500 p-6 pb-14">
+      <div className="portrait:bg-indigo-600 landscape:bg-teal-500 bg-blue-500 p-6 pb-14 xl:pb-40">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl p-6 bg-white relative -top-5">
@@ -70,7 +70,7 @@ const Home: NextPage = () => {
       </div>
 
 
-      <div className="bg-white p-10 rounded-2xl shadow-2xl">
+      <div className="bg-white p-6 rounded-3xl shadow-2xl lg:col-span-2 xl:col-span-1">
         <div className="flex mb-5 justify-between items-center">
           <span>⬅️</span>
           <div className="space-x-3">
